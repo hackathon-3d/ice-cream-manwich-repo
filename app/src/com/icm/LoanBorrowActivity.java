967f7b97a2356b1794f1068a6de61754e013308c
Matt Senn <eternalmatt@gmail.com>
2013-08-23 22:58:12 -0400

    Additonal view and hookup for loan borrow

--- app/src/com/icm/LoanBorrowActivity.java
+++ app/src/com/icm/LoanBorrowActivity.java
@@ -2,19 +2,41 @@ package com.icm;
 
 import roboguice.inject.ContentView;
 import roboguice.inject.InjectExtra;
+import roboguice.inject.InjectView;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockActivity;
 
 @ContentView(R.layout.activity_loan_borrow)
 public class LoanBorrowActivity extends RoboSherlockActivity {
     
-    @InjectExtra("title") private String title;
+    @InjectExtra("title")                   String title;
+    @InjectView(R.id.contact_name_input)    EditText contactTextView;
+    @InjectView(R.id.due_date_edit_text)    EditText dueDateTextView;
+    @InjectView(R.id.scan_button)           Button scanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setTitle(title);
+        contactTextView.setOnClickListener(contactClickListener);
+        
     }
+    
+    private final View.OnClickListener contactClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            // fire something that gets the contact info.
+        }
+    };
+    
+    private final View.OnClickListener scanButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // fire something that scans a barcode
+        }
+    };
 }
