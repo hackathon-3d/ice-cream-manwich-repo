46888696abf85534a1dc04ab043f42e2f039cba3
rendaz <rendaz@gmail.com>
2013-08-24 00:23:51 -0400

    Created Models and Urls
    Initial commit for Models and Urls

--- server/src/BooksWithFriends/src/urls.py
+++ server/src/BooksWithFriends/src/urls.py
@@ -7,18 +7,21 @@ Created on 8/23/2013
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('src', 
-    ('^getInventory/(.+)$', 'getInventory'),
-    ('^addInventoryItem/(.+)$', 'addInventoryItem'),
-    ('^getFriend/(.+)$', 'getFriend'),
-    ('^getAllFriends/(.+)$', 'getAllFriends'),
 
-    ('^getLoanedItem/(.+)$', 'getLoanedItem'),
-    ('^getAllLoanedItems/(.+)$', 'getAllLoanedItems'),
-    ('^getBorrowedItem/(.+)$', 'getBorrowedItem'),
-    ('^getAllBorrowedItems/(.+)$', 'getAllBorrowedItems'),
-    ('^newLoan/(.+)$', 'newLoan'),
-    ('^newBorrow/(.+)$', 'newBorrow'),
-    ('^getAllDueDate/(.+)$', 'getAllDueDates'),
-    ('^getUpcomingDueDates/(.+)$', 'getUpcomingDueDates'),
+    ('^newUser/(.+)$', 'new_user'),
+                       
+    ('^getInventory/(.+)$', 'get_inventory'),
+    ('^addInventoryItem/(.+)$', 'add_inventory_item'),
+    ('^getFriend/(.+)$', 'get_friend'),
+    ('^getAllFriends/(.+)$', 'get_all_friends'),
+    ('^getLoanedItem/(.+)$', 'get_loaned_item'),
+    ('^getAllLoanedItems/(.+)$', 'get_all_loaned_items'),
+    ('^getBorrowedItem/(.+)$', 'get_borrowed_item'),
+
+    ('^getAllBorrowedItems/(.+)$', 'get_all_borrowed_items'),
+    ('^newLoan/(.+)$', 'new_loan'),
+    ('^newBorrow/(.+)$', 'new_borrow'),
+    ('^getAllDueDate/(.+)$', 'get_all_due_dates'),
+    ('^getUpcomingDueDates/(.+)$', 'get_upcoming_due_dates'),
     
 )
