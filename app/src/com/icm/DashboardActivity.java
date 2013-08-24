75e3001d4c9ab986d1062430c286b62409c5bc88
Matt Senn <eternalmatt@gmail.com>
2013-08-23 23:28:55 -0400

    My code works! Loan button -> Activity

--- app/src/com/icm/DashboardActivity.java
+++ app/src/com/icm/DashboardActivity.java
@@ -1,27 +1,26 @@
 package com.icm;
 
+import roboguice.inject.InjectResource;
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 import android.view.View;
 
-public class DashboardActivity extends Activity {
+import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockActivity;
 
+public class DashboardActivity extends RoboSherlockActivity {
+
+    @InjectResource(R.string.loan_title)        String lOAN_TITLE;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dashboard);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.dashboard, menu);
-		return true;
-	}
-	
 	public void onLoanButtonClick(View view) {
-		
+		Intent intent = new Intent(this, LoanBorrowActivity.class);
+		intent.putExtra(LoanBorrowActivity.TITLE_INTENT, lOAN_TITLE);
+		startActivity(intent);
 	}
 	
 	public void onBorrowButtonClick(View view) {
